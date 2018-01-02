fe639ceac5164a79b7cb7b65109dfee805b7a53e
viralnotprasad <viral.sp@somaiya.edu>
2018-01-02 21:55:08 +0530

    Added Backpropagations gotta add a driver f(X) next

--- DR.py
+++ DR.py
@@ -25,8 +25,27 @@ def forward_pass(X,network):
 def calc_cost(O,t):
 	#AvgError
 
-def backward_pass(O,t):
-	pass
+def backward_pass(network,alpha,Weighted_sums,Inputs):
+	error=calc_error(Weighted_sums,O,t)
+	#db=error
+	#dw=[]
+	#Updating Weights and Baises
+	for i in range(len(error)):
+		dw=Inputs[i].dot(error[i])
+		db=error[i]
+		network.weights[i]+=alpha*dw
+		network.baises[i]+=alpha*db
+
+def calc_error(O,t,Weighted_sums,do):
+	do = -(t/O + (1-t)/(1-O))
+	error=[]
+	for i in range (len(Weighted_sums)):
+	error[i] = do.dot(sigmoid_gradient(Weighted_sums[i]))
+	return 0
+
+def sigmoid_gradient(z):
+	np.exp(-z)/(1.0+np.exp(-z)**2)
+
 
 #Activation Function
 def ReLu(z):
@@ -53,6 +72,7 @@ class Network:
 			self.baises.append(layer_bias)
 			Inputs=[]
 			Weighted_sums=[]
+			alpha=0.25
 
 #			my_net = Network([3,1])
 
